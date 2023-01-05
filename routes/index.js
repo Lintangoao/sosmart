cf04c3581354f72b63ac287bcfaea8531b8b5855
Lintangoao <109039578+Lintangoao@users.noreply.github.com>
2023-01-06 00:39:21 +0700

    Revert "ada"

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// router.get('/laporan', function(req, res, next) {
+//   res.render('pages/laporan');
+// });
+
 router.use('/laporan', laporan);
 
 module.exports = router;
